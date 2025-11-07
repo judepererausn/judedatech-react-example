81cdc87acb181a29d01ec19e5aadd67fc7482f5c
judepererausn <89569329+judepererausn@users.noreply.github.com>
2025-11-07 15:06:44 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ export default function App() {
   return (
     <div className="full-wrapper">
       <div className="content">
-        <h1>Jude DA Tech Node example-2</h1>
-        <p>Welcome to my modern React + Vite demo application.</p>
+        <h1>Jude DA Tech</h1>
+        <p>Welcome to my modern React host demo application.</p>
       </div>
     </div>
   );
