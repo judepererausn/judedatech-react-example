a90cb29a6ddb9c11bf5f5ecad94be896667817f6
root <root@ip-172-31-5-144.ap-south-1.compute.internal>
2025-11-07 08:37:29 +0000

    a

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <div className="full-wrapper">
+      <div className="content">
+        <h1>Jude DA Tech</h1>
+        <p>Welcome to my modern React + Vite demo application.</p>
+      </div>
+    </div>
+  );
+}
+
